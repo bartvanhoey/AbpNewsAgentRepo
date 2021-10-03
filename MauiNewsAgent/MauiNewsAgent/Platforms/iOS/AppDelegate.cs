f51803d5f33ddfcfc1402f37e5200c454df76c17
bartvanhoey <bartvanhoey@hotmail.com>
2021-10-03 20:35:13 +0200

    MauiAppAdded

---
+++ MauiNewsAgent/MauiNewsAgent/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MauiNewsAgent
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
